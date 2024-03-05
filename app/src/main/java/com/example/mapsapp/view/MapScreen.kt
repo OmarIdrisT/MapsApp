16a34b9b6bdead7e61d1c9df68476a6691868adb
OmarIdrisT <omar.idris.7e7@itb.cat>
2024-03-05 13:56:07 +0100

    Mapa funciona pero sale en blanco

--- app/src/main/java/com/example/mapsapp/view/MapScreen.kt
+++ app/src/main/java/com/example/mapsapp/view/MapScreen.kt
@@ -10,6 +10,11 @@ import androidx.navigation.NavHostController
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
@@ -18,7 +23,7 @@ fun MapScreen(navigationController: NavHostController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(8.dp)
     ) {
         val itb = LatLng(41.4534265, 2.1837151)
         val cameraPositionState = rememberCameraPositionState {
@@ -26,8 +31,19 @@ fun MapScreen(navigationController: NavHostController) {
         }
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
-        )
+            cameraPositionState = cameraPositionState,
+            uiSettings = MapUiSettings(compassEnabled = true, indoorLevelPickerEnabled = true, rotationGesturesEnabled = true, zoomControlsEnabled = true),
+            properties = MapProperties(mapType = MapType.HYBRID),
+            onMapClick = {},
+            onMapLongClick = {
+            }
+        ) {
+            Marker(
+                state = MarkerState(position = itb),
+                title = "ITB",
+                snippet = "Marker at ITB"
+            )
+        }
 
     }
 
